f7c45926e9b7b7f0235bb685bf55969ebe8260a9
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-06-13 22:46:52 -0500

    feat(rust): initialize rust workspace

--- flake-parts/shells.nix
+++ flake-parts/shells.nix
@@ -8,15 +8,30 @@
     lib,
     ...
   }: let
-    inherit (self'.packages) factorio-wrapper;
+    inherit (self'.packages) rust-toolchain factorio-wrapper;
+    inherit (self'.legacyPackages) cargoExtraPackages ciPackages;
 
     devTools = [
       factorio-wrapper
+      # rust tooling
+      rust-toolchain
+      pkgs.cargo-audit
+      pkgs.cargo-udeps
+      pkgs.bacon
+      # formatting
+      self'.packages.treefmt
     ];
   in {
     devShells = {
-      default = pkgs.mkShell {
-        packages = devTools;
+      default = pkgs.mkShell rec {
+        packages = devTools ++ cargoExtraPackages ++ ciPackages;
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+        RUST_SRC_PATH = "${self'.packages.rust-toolchain}/lib/rustlib/src/rust/src";
+
+        shellHook = ''
+          ${config.pre-commit.installationScript}
+        '';
       };
     };
   };
