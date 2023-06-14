2cc0d96dc48b16378a61f4ffcfafce1cf9d6fd73
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-06-13 22:46:52 -0500

    feat(rust): initialize rust workspace

--- flake.nix
+++ flake.nix
@@ -2,6 +2,23 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-filter.url = "github:numtide/nix-filter";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    bomper = {
+      url = "github:justinrubek/bomper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
@@ -12,6 +29,13 @@
         ./flake-parts/factorio.nix
         ./flake-parts/dockerImages.nix
         ./flake-parts/ci.nix
+
+        ./flake-parts/cargo.nix
+        ./flake-parts/rust-toolchain.nix
+
+        ./flake-parts/formatting.nix
+        ./flake-parts/pre-commit.nix
+        inputs.pre-commit-hooks.flakeModule
       ];
     };
 }
