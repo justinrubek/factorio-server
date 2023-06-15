67ecd55473a57ea72cfd8767de20c36648a6b4a0
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-06-15 13:00:49 -0500

    feat(cli): add download single-mod command
    The command will retrieve the details of the mod, find the selected
    version, and download the file into the current directory. Types for the
    relevant APIs have been added so that the functionality may be shared
    with an upcoming feature that will accept a mod list and download all of
    the mods.

--- crates/cli/src/error.rs
+++ crates/cli/src/error.rs
@@ -2,6 +2,8 @@
 pub enum Error {
     #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -3,6 +3,9 @@ use crate::{
     error::Result,
 };
 use clap::Parser;
+use commands::{DownloadCommands, ModDetails};
+use tokio::io::AsyncWriteExt;
+use tracing::info;
 
 mod commands;
 mod error;
@@ -34,6 +37,69 @@ async fn main() -> Result<()> {
 
                     let body = res.json::<factorio_api::LoginResponse>().await?;
                     println!("{}", body.token);
+                }
+            }
+        }
+        Commands::Download(download) => {
+            let cmd = download.command;
+            match cmd {
+                DownloadCommands::SingleMod(ModDetails { name, version }) => {
+                    info!("Downloading mod {name} version {version}");
+                    // Retrieve the mod's info
+                    let client = reqwest::Client::new();
+
+                    let res = client
+                        .get(format!("https://mods.factorio.com/api/mods/{name}"))
+                        .send()
+                        .await?;
+
+                    match res.status() {
+                        reqwest::StatusCode::OK => (),
+                        _ => {
+                            let body = res.json::<factorio_api::ErrorResponse>().await?;
+                            tracing::error!("{:#?}", body);
+                            return Ok(());
+                        }
+                    }
+
+                    let body = res.json::<factorio_api::ModResponse>().await?;
+                    // determine the download_url using the version
+                    let (download_url, file_name) = body
+                        .releases
+                        .iter()
+                        .find(|release| release.version == version)
+                        .map(|release| (release.download_url.clone(), release.file_name.clone()))
+                        .unwrap_or_else(|| {
+                            tracing::error!("Version {} not found for mod {}", version, name);
+                            std::process::exit(1);
+                        });
+
+                    tracing::debug!("Downloading from {}", download_url);
+
+                    // Retrieve auth token and username from environment variables
+                    let factorio_username =
+                        std::env::var("FACTORIO_USERNAME").expect("FACTORIO_USERNAME not set");
+                    let factorio_token =
+                        std::env::var("FACTORIO_TOKEN").expect("FACTORIO_TOKEN not set");
+
+                    // Retrieve the mod's file
+                    let res = client.get(format!("https://mods.factorio.com{download_url}?username={factorio_username}&token={factorio_token}"))
+                        .send()
+                        .await?;
+
+                    match res.status() {
+                        reqwest::StatusCode::OK => (),
+                        _ => {
+                            let body = res.json::<factorio_api::ErrorResponse>().await?;
+                            tracing::error!("{:#?}", body);
+                            return Ok(());
+                        }
+                    }
+
+                    let body = res.bytes().await?;
+                    // Write the file to disk
+                    let mut file = tokio::fs::File::create(file_name).await?;
+                    file.write_all(&body).await?;
                 }
             }
         }
