e24cabf513b29057cee9a6cadcb67f90f5680d36
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-10-21 14:00:59 -0500

    fixup! fix: update factorio-versions

--- flake-parts/factorio.nix
+++ flake-parts/factorio.nix
@@ -20,16 +20,16 @@
     factorio-config = pkgs.writeText "factorio.conf" ''
       use-system-read-write-data-directories=true
       [path]
-      read-data=${self'.packages.factorio-pkg}/share/factorio/data
+      read-data=${self'.packages.factorio-headless}/share/factorio/data
       write-data=./.factorio
     '';
 
     factorio-wrapper = pkgs.writeShellApplication {
       name = "factorio";
-      runtimeInputs = [self'.packages.factorio-pkg];
+      runtimeInputs = [self'.packages.factorio-headless];
       text = ''
         #!/usr/bin/env bash
-        exec ${self'.packages.factorio-pkg}/bin/factorio \
+        exec ${self'.packages.factorio-headless}/bin/factorio \
           --config ${factorio-config} \
           "$@"
       '';
