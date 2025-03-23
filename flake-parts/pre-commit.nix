4d6a6fc6db0f5b646b4996c61037d353c4449a42
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-03-23 09:04:45 -0500

    chore: cleanup configuration

--- flake-parts/pre-commit.nix
+++ flake-parts/pre-commit.nix
@@ -1,20 +1,16 @@
 {
-  inputs,
-  self,
-  ...
-}: {
-  perSystem = {self', ...}: let
-  in {
+  perSystem = {self', ...}: {
     pre-commit = {
       check.enable = true;
 
       settings = {
         src = ../.;
         hooks = {
-          treefmt.enable = true;
+          treefmt = {
+            enable = true;
+            package = self'.packages.treefmt;
+          };
         };
-
-        settings.treefmt.package = self'.packages.treefmt;
       };
     };
   };
